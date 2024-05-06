fd13637c8109f635a682ff183d2796de4af42cea
psergioPadilha <psergiopadilha@gmail.com>
2024-05-06 11:51:35 -0300

    Atualização RepositorioEntrada

---
+++ ControleMedicamentos.ConsoleApp/ModuloRequisicaoEntrada/RequisicaoEntrada.cs
@@ -0,0 +1,57 @@
+﻿using ControleMedicamentos.ConsoleApp.Compartilhado;
+using ControleMedicamentos.ConsoleApp.ModuloMedicamento;
+using ControleMedicamentos.ConsoleApp.ModuloFornecedor;
+
+namespace ControleMedicamentos.ConsoleApp.ModuloRequisicao
+{
+    internal class RequisicaoEntrada : EntidadeBase
+    {
+
+        public Medicamento medicamento { get; set; }
+        public Fornecedor fornecedor { get; set; }
+        public DateTime dataRequisicaoEntrada { get; set; }
+        public int quantidadeEntrada { get; set; }
+
+        public RequisicaoEntrada(Medicamento medicamentoSelecionado, Fornecedor fornecedorSelecionado, int quantidade)
+        {
+            this.medicamento = medicamentoSelecionado;
+            this.fornecedor = fornecedorSelecionado;
+            this.dataRequisicaoEntrada = DateTime.Now;
+            this.quantidadeEntrada = quantidade;
+        }
+
+        public override string[] Validar()
+        {
+            string[] erros = new string[3];
+            int contadorErros = 0;
+
+            if (medicamento == null)
+                erros[contadorErros++] = "O medicamento precisa ser preenchido";
+
+            if (fornecedor == null)
+                erros[contadorErros++] = "O fornecedor precisa ser informado";
+
+            if (quantidadeEntrada < 1)
+                erros[contadorErros++] = "Por favor informe uma quantidade válida";
+
+            string[] errosFiltrados = new string[contadorErros];
+
+            Array.Copy(erros, errosFiltrados, contadorErros);
+
+            return errosFiltrados;
+        }
+
+        public bool AcrescentarMedicamento()
+        {
+            medicamento.Quantidade += quantidadeEntrada;
+            return true;
+        }
+    }
+}
+
+//Registrar Requisição de Entrada:
+//
+//O usuário poderá fazer uma requisição de entrada de medicamento que incluirá:
+    //data da requisição, dados do medicamento, dados do fornecedor, dados do funcionário e a quantidade requisitada do medicamento.
+    //A quantidade do medicamento deve ser atualizada.
+    //Visualizar Requisições de Entrada: Exibe uma lista exibindo detalhes de todas as requisições de Entrada registradas.
---
+++ ControleMedicamentos.ConsoleApp/ModuloRequisicaoEntrada/TelaRequisicaoEntrada.cs
@@ -0,0 +1,101 @@
+﻿using ControleMedicamentos.ConsoleApp.Compartilhado;
+using ControleMedicamentos.ConsoleApp.ModuloFuncionario;
+using ControleMedicamentos.ConsoleApp.ModuloMedicamento;
+using ControleMedicamentos.ConsoleApp.ModuloPaciente;
+
+namespace ControleMedicamentos.ConsoleApp.ModuloRequisicao
+{
+    internal class TelaRequisicaoEntrada : TelaBase
+    {
+        public TelaFornecedor telaFornecedor = null;
+        public TelaMedicamento telaMedicamento = null;
+        public RepositorioFornecedor repositorioFornecedor = null;
+        public RepositorioMedicamento repositorioMedicamento = null;
+
+        public override void Registrar()
+        {
+            ApresentarCabecalho();
+
+            Console.WriteLine($"Cadastrando {tipoEntidade}...");
+
+            Console.WriteLine();
+
+            RequisicaoEntrada entidade = (RequisicaoEntrada)ObterRegistro();
+
+            string[] erros = entidade.Validar();
+
+            if (erros.Length > 0)
+            {
+                ApresentarErros(erros);
+                return;
+            }
+
+            bool conseguiuAcrescentar = entidade.AcrescentarMedicamento();
+
+            repositorio.Cadastrar(entidade);
+
+            ExibirMensagem($"O {tipoEntidade} foi cadastrado com sucesso!", ConsoleColor.Green);
+        }
+
+        public override void VisualizarRegistros(bool exibirTitulo)
+        {
+            if (exibirTitulo)
+            {
+                ApresentarCabecalho();
+
+                Console.WriteLine("Visualizando Requisições de Entrada...");
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine(
+                "{0, -10} | {1, -15} | {2, -15} | {3, -20} | {4, -5}",
+                "Id", "Medicamento", "Fornecedor", "Data de Requisição", "Quantidade"
+            );
+
+            EntidadeBase[] requisicoesCadastradas = repositorio.SelecionarTodos();
+
+            foreach (RequisicaoSaida requisicao in requisicoesCadastradas)
+            {
+                if (requisicao == null)
+                    continue;
+
+                Console.WriteLine(
+                    "{0, -10} | {1, -15} | {2, -15} | {3, -20} | {4, -5}",
+                    requisicao.Id,
+                    requisicao.medicamento.Nome,
+                    requisicao.paciente.Nome,
+                    requisicao.dataRequisicao.ToShortDateString(),
+                    requisicao.quantidadeAcrescentada
+                );
+            }
+
+            Console.ReadLine();
+            Console.WriteLine();
+        }
+
+        protected override EntidadeBase ObterRegistro()
+        {
+            telaMedicamento.VisualizarRegistros(false);
+
+            Console.Write("Digite o ID do medicamento: ");
+            int idMedicamento = Convert.ToInt32(Console.ReadLine());
+
+            Medicamento medicamentoSelecionado = (Medicamento)repositorioMedicamento.SelecionarPorId(idMedicamento);
+
+            telaPaciente.VisualizarRegistros(false);
+
+            Console.Write("Digite o ID do fornecedor: ");
+            int idPaciente = Convert.ToInt32(Console.ReadLine());
+
+            Fornecedor fornecedorSelecionado = (Fornecedor)repositorioFornecedor.SelecionarPorId(idFornecedor);
+
+            Console.Write("Digite a quantidade de entrada de medicamente: ");
+            int quantidade = Convert.ToInt32(Console.ReadLine());
+
+            RequisicaoSaida novaRequisicao = new RequisicaoEntrada(medicamentoSelecionado, fornecedorSelecionado, quantidade);
+
+            return novaRequisicao;
+        }
+    }
+}
